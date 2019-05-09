a3d59840b4e58d31df2ad835f604a177ad65329b
Johan Sutrisno <johansutrisno30@gmail.com>
2019-05-09 12:58:59 +0700

    Change koneksi.php
    Mengubah koneksi.php

--- index.php
+++ index.php
@@ -5,5 +5,24 @@
 </head>
 <body>
 	Testing
+	<?php
+	    $serverName = "johandicodingwebappserver.database.windows.net"; // update me
+	    $connectionOptions = array(
+	        "Database" => "dicodingwebapp", // update me
+	        "Uid" => "dicoding", // update me
+	        "PWD" => "Johan300998" // update me
+	    );
+	    //Establishes the connection
+	    $conn = sqlsrv_connect($serverName, $connectionOptions);
+	    $tsql= "Select * FROM [dbo].[user]";
+	    $getResults= sqlsrv_query($conn, $tsql);
+	    echo ("Reading data from table" . PHP_EOL);
+	    if ($getResults == FALSE)
+	        echo (sqlsrv_errors());
+	    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+	     echo ($row['ID'] . " " . $row['NAME'] . PHP_EOL);
+	    }
+	    sqlsrv_free_stmt($getResults);
+	?>
 </body>
 </html>
--- koneksi.php
+++ koneksi.php
@@ -7,16 +7,13 @@
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    // $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
-    //      FROM [SalesLT].[ProductCategory] pc
-    //      JOIN [SalesLT].[Product] p
-    //      ON pc.productcategoryid = p.productcategoryid";
-    // $getResults= sqlsrv_query($conn, $tsql);
-    // echo ("Reading data from table" . PHP_EOL);
-    // if ($getResults == FALSE)
-    //     echo (sqlsrv_errors());
-    // while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    //  echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
-    // }
-    // sqlsrv_free_stmt($getResults);
+    $tsql= "Select * FROM [dbo].[user]";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['ID'] . " " . $row['NAME'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
 ?>

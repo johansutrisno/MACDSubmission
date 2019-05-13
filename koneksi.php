fea16b38e3525c88a7f071ca4dfb32fdf5d5afa2
Johan Sutrisno <johansutrisno30@gmail.com>
2019-05-13 16:18:34 +0700

    Update ssl

--- koneksi.php
+++ koneksi.php
@@ -4,11 +4,20 @@
 	$pass = "Johan300998";
 	$db = "sayurandb";
 
-	$koneksi = mysqli_connect($server,$user,$pass,$db);
+	$koneksi = mysqli_init();
+
+	mysqli_ssl_set($conn,NULL,NULL, "/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+	mysqli_real_connect($conn, $server, $user, $pass, $db, 3306, MYSQLI_CLIENT_SSL);
+
+	if (mysqli_connect_errno($conn)) {
+		die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
+
+	// $koneksi = mysqli_connect($server,$user,$pass,$db);
 
 	// Check connection
-	if (mysqli_connect_errno()){
-		echo "Koneksi database gagal : " . mysqli_connect_error();
-	}
+	// if (mysqli_connect_errno()){
+		// echo "Koneksi database gagal : " . mysqli_connect_error();
+	// }
 
 ?>
